2b1262ca7fab8b3cf23cac9aaa35c91dfcb40d4f
aseongjun99 <aseongjun99@gmail.com>
2025-04-30 17:42:56 +0900

    feat: url 지정

--- backend/notification-service/app/main.py
+++ backend/notification-service/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.api.email_api import router as email_router
 
-app = FastAPI()
+# app = FastAPI()
+
+
+app = FastAPI(docs_url="/api/notification/docs", redoc_url=None, openapi_url="/api/notification/openapi.json")
+# app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG, lifespan=lifespan, docs_url=None, redoc_url=None, openapi_url=None) # 배포 시 docs 비활성화
 
 app.include_router(email_router)
