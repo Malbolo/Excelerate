bff4af74e1611ac79b23f3f64750ac99d44117a0
김동철 <csct3434@gmail.com>
2025-05-03 21:18:45 +0900

    feat(user-service): JWT 토큰 발급 유틸리티 추가

--- backend/user-service/src/main/java/io/ssafy/p/k12s101/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/io/ssafy/p/k12s101/userservice/UserServiceApplication.java
@@ -2,12 +2,14 @@ package io.ssafy.p.k12s101.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class UserServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
 
 }
