6d4e8b6f364b24a63e81754e517d5eec047e19e4
aseongjun99 <aseongjun99@gmail.com>
2025-05-05 00:50:27 +0900

    feat: 테이블 생성

--- backend/job-service/app/main.py
+++ backend/job-service/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.api.job_api import router as job_router
+from app.db.database import Base, engine
 
 # app = FastAPI()
 app = FastAPI(docs_url="/api/jobs/docs", redoc_url=None, openapi_url="/api/jobs/openapi.json")
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(job_router)
