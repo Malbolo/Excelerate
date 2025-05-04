295e37df489a600e262bb132493401b9f542aa92
aseongjun99 <aseongjun99@gmail.com>
2025-05-04 23:13:02 +0900

    feat: dto 추가

---
+++ backend/job-service/app/schemas/job_create_schema.py
@@ -0,0 +1,15 @@
+from typing import List
+from pydantic import BaseModel
+
+class JobCreateRequest(BaseModel):
+    type: str
+    name: str
+    description: str
+    data_load_command: str
+    data_load_url: str
+    commands: List[str]
+    code: str
+
+class JobCreateResponse(BaseModel):
+    result: str
+    data : dict
