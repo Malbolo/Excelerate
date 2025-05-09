45a4426a4dcf40fd8c1957976ec80f3186f92e59
안성준 <aseongjun99@gmail.com>
2025-05-09 11:24:24 +0900

    refactor: 메서드 분리

--- backend/job-service/app/services/job_service.py
+++ backend/job-service/app/services/job_service.py
@@ -1,39 +1,48 @@
-import logging
 import math
-import requests
+from sqlalchemy.orm.exc import NoResultFound
 from typing import Optional
 from fastapi import HTTPException
 from sqlalchemy.orm import Session, joinedload
 from starlette.responses import JSONResponse
+from app.core.log_config import logger
 from app.crud import crud
 from app.models import models
-from app.schemas import job_detail_schema, job_list_schema
-from app.schemas.job_create_schema import JobCreateRequest
-from app.schemas.job_detail_schema import JobDetailResponse
-from app.schemas.job_update_schema import JobUpdateRequest
+from app.schemas.job import job_detail_schema, job_list_schema, job_create_schema
+from app.schemas.job.job_create_schema import JobCreateRequest
+from app.schemas.job.job_detail_schema import JobDetailResponse, JobDetailRequest
+from app.schemas.job.job_update_schema import JobUpdateRequest
 from app.core import auth
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 async def create_job(request: JobCreateRequest, user_id: int, db: Session) -> JSONResponse:
     try:
         user_info = auth.get_user_info(user_id)
+        job = await crud.create_job(db, request, user_id, user_info.get("name"), user_info.get("department"))
 
-        await crud.create_job(db, request, user_id, user_info.get("name"), user_info.get("department"))
-        return JSONResponse(status_code=200, content={"message": "Job이 생성되었습니다."})
+        data = job_create_schema.JobCreateResponseData(
+            job_id=str(job.id),
+            created_at=str(job.created_at)
+        )
+        response = job_create_schema.JobCreateResponse(result="success", data=data)
+        return JSONResponse(content=response.dict())
+
     except Exception as e:
-        logger.error(f"Failed to create job for user_id {user_id}: {e}", exc_info=True)  # 에러 로그 (예외 정보 포함)
-        return JSONResponse(status_code=500, content={"message": "Job이 생성에 실패하였습니다."})
+        logger.debug(f"Job Creation Failed: {e}")
+        response = job_create_schema.JobCreateResponse(result="fail", data=None)
+        return JSONResponse(content=response.dict())
 
-async def get_job_detail(id: str, db: Session) -> JSONResponse:
-    job = db.query(models.Job).filter(models.Job.id == id).first()
-    if not job:
+async def get_job_detail(job_id: str, db: Session) -> JSONResponse:
+    try:
+        job = crud.get_job_detail_by_id(db, job_id)
+        job_data = job_detail_schema.create_job_detail_schema(job)
+
+        response = JobDetailResponse(result="success", data=job_data)
+        return JSONResponse(content=response.dict())
+    except NoResultFound:
         raise HTTPException(status_code=404, detail="Job not found")
-
-    job_data = job_detail_schema.create_job_detail_schema(job)
-
-    return JSONResponse(content=JobDetailResponse(result="success", data=job_data).dict())
+    except Exception as e:
+        logger.debug(f"Getting A Job Detail Failed: {e}")
+        response = JobDetailResponse(result="fail", data=None)
+        return JSONResponse(content=response.dict())
 
 async def update_job(db: Session, job_id: int, job_request: JobUpdateRequest, user_id: int):
     return crud.update_job(db, job_id, job_request, user_id)
@@ -41,53 +50,61 @@ async def update_job(db: Session, job_id: int, job_request: JobUpdateRequest, us
 def delete_job(db: Session, job_id: int, user_id: int):
     return crud.delete_job(db, job_id, user_id)
 
-def get_filtered_query(db: Session, mine: bool, name: Optional[str], dep: Optional[str], type: Optional[str], title: Optional[str], user_id: int):
+def filter_query(request: JobDetailRequest, query):
+    if request.name:
+        query = query.filter(models.Job.user_name.ilike(f"%{request.name}%"))
+    if request.dep:
+        query = query.filter(models.Job.user_department == request.dep)
+    if request.type:
+        query = query.filter(models.Job.type == request.type)
+    if request.title:
+        query = query.filter(models.Job.title.ilike(f"%{request.title}%"))
+    return query
+
+def is_admin_user(user_info):
+    if user_info is None or user_info.get("role") != "ADMIN":
+        return False
+    return True
+
+def get_filtered_query(db: Session, request: JobDetailRequest, user_id: int):
     query = db.query(models.Job).options(joinedload(models.Job.commands))
-    if mine:
+    if request.mine:
         query = query.filter(models.Job.user_id == user_id)
     else:
         user_info = auth.get_user_info(user_id)
-        if user_info is None or user_info.get("role") != "ADMIN":
-            raise HTTPException(
-                status_code=403,
-                detail="접근 권한이 없습니다. (관리자만 접근 가능)"
-            )
+        if not is_admin_user(user_info):
+            raise HTTPException(status_code=401, detail="관리자만 접근 가능합니다.")
 
-        if name:
-            query = query.filter(models.Job.user_name.ilike(f"%{name}%"))
-
-        if dep:
-            query = query.filter(models.Job.user_department == dep)
-
-    if type:
-        query = query.filter(models.Job.type == type)
-
-    if title:
-        query = query.filter(models.Job.title.ilike(f"%{title}%"))
-
-    return query
+    return filter_query(request, query)
 
 
-def get_jobs(db: Session, mine: bool, name: Optional[str], dep: Optional[str], type: Optional[str], page: Optional[int], size: Optional[int], title: Optional[str], user_id: int):
-    query = get_filtered_query(db, mine, name, dep, type, title, user_id)
-
+def get_total_page_count(query_count: int, size: int):
     if size:
-        total = math.ceil(query.count() / size)
-    else:
-        total = 1
+        return math.ceil(query_count / size)
+    return 1
 
+def paginate_query(query, page: int | None, size: int | None):
     if page is not None and size is not None:
-        jobs = query.offset((page - 1) * size).limit(size).all()
-    else:
-        jobs = query.all()
+        return query.offset((page - 1) * size).limit(size).all()
+    return query.all()
 
+
+def get_jobs(db: Session, request: JobDetailRequest, user_id: int):
+    query = get_filtered_query(db, request, user_id)
+
+    total = get_total_page_count(query.count(), request.size)
+
+    jobs = paginate_query(query, request.page, request.size)
     job_data = [job_detail_schema.create_job_detail_schema(job) for job in jobs]
 
-    return JSONResponse(content=job_list_schema.JobListResponse(
+    response = job_list_schema.JobListResponse(
         result="success",
         data={
             "jobs": job_data,
-            "page": page,
-            "size": size,
+            "page": request.page,
+            "size": request.size,
             "total": total
-        }).dict())
+        }
+    )
+
+    return JSONResponse(content=response.dict())
