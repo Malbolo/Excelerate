807f16fa9a27a7ecd0289885efb0530abc701292
aseongjun99 <aseongjun99@gmail.com>
2025-05-13 17:21:46 +0900

    test: 테스트를 위한 커밋

--- backend/schedule-service/app/main.py
+++ backend/schedule-service/app/main.py
@@ -4,6 +4,7 @@ from app.db.database import Base, engine
 from app.api.schedule_api import router as schedule_router
 
 # app = FastAPI()
+
 app = FastAPI(docs_url="/api/schedulers/docs", redoc_url=None, openapi_url="/api/schedulers/openapi.json")
 
 # Base.metadata.create_all(bind=engine)
